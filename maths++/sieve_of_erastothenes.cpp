b0f6ed9f2a15e38d11be6638e18e82f3512ed013
Abhist <abhistt1602@gmail.com>
2023-01-19 21:05:12 +0530

    dop

--- maths++/sieve_of_erastothenes.cpp
+++ maths++/sieve_of_erastothenes.cpp
@@ -2,22 +2,29 @@
 using namespace std;
 int main()
 {
-    bool prime[101];
+    int mx=1e5;
+    bool prime[mx+1];
     memset(prime,true,sizeof(prime));
-    prime[1]=false;
-    for(int i=2;i*i<=100;i++)
-    {
-        for(int j=i*i;j<=100;j+=i)
-        {
-            prime[j]=false;
-        }
-    }
-    for(int i=1;i<=100;i++)
+    prime[0]=false,prime[1]=false;
+    for(int i=2;i*i<=mx;i++)
     {
         if(prime[i])
         {
+            for(int j=i*i;j<=mx;j+=i)
+            {
+                prime[j]=false;
+            }
+        }
+    }
+    int ct=0;
+    for(int i=1;i<=mx;i++)
+    {
+        if(prime[i])
+        {
+            ct++;
             cout<<i<<' ';
         }
     }
     cout<<'\n';
+    cout<<ct<<'\n';
 }
